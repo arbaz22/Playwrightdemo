d41a122baa1c205e45d98c6532d9c41d1905f5f9
Arbaz <arbazahamed91@gmail.com>
2025-08-09 08:46:59 +0530

    config file changes

--- playwright.config.js
+++ playwright.config.js
@@ -39,16 +39,16 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'],
-    //     screenshot:"on",
-    //     video:"on",
-    //     trace:"on"
+     {
+      name: 'chromium',
+       use: { ...devices['Desktop Chrome'],
+         screenshot:"on",
+         video:"on",
+         trace:"on"
 
-    //   },
+       },
       
-    // },
+     },
 
       {
        name: 'firefox',
